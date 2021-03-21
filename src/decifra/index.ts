18ef9f6cea8ee101ed8ba0c0022255246e789400
felipelealdefaria <felipelealdefaria@gmail.com>
2021-03-21 20:30:27 -0300

    chore: get all cli params

--- src/decifra/index.ts
+++ src/decifra/index.ts
@@ -3,14 +3,19 @@ import path from 'path'
 import { decifra } from './decifra'
 
 const fileText = process.argv[2]
+const fileWrite = process.argv[3]
 
-const filePath = path.isAbsolute(fileText) ? fileText : path.join(__dirname, fileText)
+const pathRoot = process.cwd() + '/src/decifra/tmp/'
+
+const filePath = path.isAbsolute(fileText) ? fileText : path.join(pathRoot, fileText)
 const file = fs.readFileSync(filePath, { encoding: 'utf-8' })
 
-const encript = decifra(file)
-const { textDecoded, key } = encript
+const decode = decifra(file)
+const { textDecoded, key } = decode
 
-fs.writeFile('tmp/texto-aberto.txt', textDecoded, (err) => {
+const text = `Key: ${key} \nText Decoded: ${textDecoded}`
+
+fs.writeFile(`${pathRoot}${fileWrite}`, text, (err) => {
   if (err) return console.log(err)
-  console.log('The file was saved in tmp/text-aberto.txt')
+  console.log(`The file was saved in src/decifra/tmp/${fileWrite}`)
 })
